1f643da9adf31f65a2b45fc33aa6f10b5f74267f
Ghoza <ghoza60@gmail.com>
2024-11-26 17:14:57 +0700

    initial commit

--- src/app/client.ts
+++ src/app/client.ts
@@ -1,5 +1,5 @@
-import { createThirdwebClient } from "thirdweb";
-
+import { createThirdwebClient, getContract } from "thirdweb";
+import { defineChain } from "thirdweb/chains";
 // Replace this with your client ID string
 // refer to https://portal.thirdweb.com/typescript/v5/client on how to get a client ID
 const clientId = process.env.NEXT_PUBLIC_TEMPLATE_CLIENT_ID;
@@ -10,4 +10,9 @@ if (!clientId) {
 
 export const client = createThirdwebClient({
   clientId: clientId,
+});
+export const contract = getContract({
+  client,
+  chain: defineChain(4202),
+  address: "0x96588D0Ea7E21cd685DbD71DCCaEb5519A759e28",
 });
