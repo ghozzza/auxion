4593f73258e70d374bffa271fbf1549a67f79dec
Ghoza <ghoza60@gmail.com>
2024-11-28 18:15:33 +0700

    fix: calling function balance

--- src/app/balance/page.tsx
+++ src/app/balance/page.tsx
@@ -1,3 +1,4 @@
+"use client"
 import Container from '@/components/Container'
 import React from 'react'
 import { useReadContract } from "thirdweb/react";
@@ -6,18 +7,19 @@ import { contract } from "../client";
 
 
 const Balance = () => {
-    // const { data, isPending } = useReadContract({
-    //     contract,
-    //     method:
-    //       "function balances(address) view returns (uint256)",
-    //     params: [],
-    //   });
+    const { data, isPending } = useReadContract({
+        contract,
+        method:
+          "function balances(address) view returns (uint256)",
+        params: [],
+      });
   return (
     <Container>
         <h1 className="text-center text-4xl">
             Current Balance
         </h1>
-        {/* {data} */}
+        {data}
+        {isPending}
     </Container>
   )
 }
